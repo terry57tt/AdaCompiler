739369c1cddea0b2e73ccfc1488e35711b80fa30
Zhen Julie <julie.zhen@telecomnancy.eu>
2023-10-25 23:13:53 +0200

    correction filehandler, getCharacter : ajout du \n

--- src/main/java/org/pcl/FileHandler.java
+++ src/main/java/org/pcl/FileHandler.java
@@ -10,14 +10,14 @@ import java.nio.file.Files;
 public class FileHandler {
 
     /** Check if the path is valid and the file exists. */
-    public boolean isPathValid(String path) {
+    public static boolean isPathValid(String path) {
         Path chemin = Paths.get(path);
         return Files.exists(chemin);
     }
 
     /** Check is the extension of the file is valid.
      * ADA extension are [.ads .adb]*/
-    public boolean isExtensionValid(String path) {
+    public static boolean isExtensionValid(String path) {
         Path chemin = Paths.get(path);
         String fileName = chemin.getFileName().toString();
         String extension = getExtension(fileName);
@@ -28,16 +28,23 @@ public class FileHandler {
     }
 
     /** Get the extension in a fileName */
-    public String getExtension(String fileName){
+    public static String getExtension(String fileName){
         String[] parties = fileName.split(".");
         return parties[parties.length-1];
     }
 
     /** Return the character stream of the file */
-    public Stream<Character> getCharacters(String file) throws IOException { //file can be a path or a file name
-        Stream<String> lines = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.defaultCharset())).lines();
-        Stream<Character> characterStream = lines.flatMap(str -> str.chars().mapToObj(c -> (char) c));
+    public static Stream<Character> getCharacters(String path) throws IOException { //file can be a path or a file name
+        //Stream<String> lines = new BufferedReader(new InputStreamReader(new FileInputStream(path), Charset.defaultCharset())).lines();
+        //Stream<Character> characterStream = lines.flatMap(str -> str.chars().mapToObj(c -> (char) c));
+        Path cheminPath = Paths.get(path);
+        Stream<String> lines = Files.lines(cheminPath, StandardCharsets.UTF_8);
+        Stream<Character> characterStream = lines.flatMap(str -> Stream.concat(str.chars().mapToObj(c -> (char) c), Stream.of('\n')));
         return characterStream;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(getCharacters("test.txt"));
     }
 
 }
