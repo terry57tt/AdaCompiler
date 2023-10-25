48ba5c2221d71a6fcf3160d8edea7c2392482c05
Mathias-AURAND <114230623+Mathias-AURAND@users.noreply.github.com>
2023-10-25 18:04:05 +0200

    debut lexeur

--- src/main/java/org/pcl/Lexeur.java
+++ src/main/java/org/pcl/Lexeur.java
@@ -1,5 +1,10 @@
 package org.pcl;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+
+import org.pcl.structure.automaton.Graph;
+import org.pcl.structure.automaton.Automaton;
+import org.pcl.structure.automaton.TokenType;
 
 public class Lexeur {
     //Attributs 
@@ -10,4 +15,17 @@ public class Lexeur {
     FileHandler fileHandler = new FileHandler();
     Stream<Character> characterStream = fileHandler.getCharacters(path);
 
+    /*Fonction qui renvoit une liste de token : la fonction va parcourir le tableau 
+     * de stream<caracter> va parcourir les transitions du graphes et si il voit 
+     * un espace ou un separateur il va creer un token et le rajouter dans la liste de token
+     * qui sera crée progressivement. Le token sera un triplet (type, valeur, ligne)
+    */
+    public ArrayList<Token> getTokens() {
+        ArrayList<Token> tokens = new ArrayList<>();
+        Graph graph = new Graph();
+        Automaton automaton = graph.create();
+        //Tant que le prochain caractère de la stream n'est pas null
+        }
 }
+
+
